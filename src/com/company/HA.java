0109830b4aed36166eb6a24f40edae9ae2a4e2eb
Siropchik007 <stas.koval.89@mail.ru>
2022-03-04 15:32:43 +0300

    Inw

---
+++ src/com/company/HA.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class HA {
+    public int d = 2;
+
+    public void asd(){
+        String sobaka = "Ball";
+        System.out.print(sobaka);
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 8;
+	    System.out.println("Helly Stasyan");
+        while (a>5){
+            System.out.println("A = " + a);
+            a = a-1;
+        }
+        HA chel = new HA();
+        chel.d =14;
+        System.out.println(chel.d);
+        chel.asd();
+
+    }
+}
