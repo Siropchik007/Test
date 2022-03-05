444ad69a3e525c0493e50ea743f7b4563cf7d7f4
Siropchik007 <stas.koval.89@mail.ru>
2022-03-05 19:06:45 +0300

    5 урок

--- src/test/java/lesson5.java
+++ src/test/java/lesson5.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 //переключение на др.тип циклов, в котором инициал-ся общий контекст для тестов данного класса
 //нужно быть осторожней, если есть static пер.класса (?)
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Tag("lesson5_6")
 public class  lesson5{
 
     @Test
@@ -20,13 +26,80 @@ public class  lesson5{
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 5, 8, 4})
-    public void ParamsTest(int age){
+    @MethodSource
+    public void TestAge(Age age, String type){
         String dog = "Jack ";
         String breed  = "Chihuahua ";
         System.out.println("Имя: "+ dog);
         System.out.println("Порода: "+ breed);
-        System.out.println("Возраст: "+ age);
+        System.out.println("Возраст: " + age +" " + type);
+    }
+
+    static Stream<Arguments> TestAge(){
+        return Stream.of(
+                Arguments.arguments(Age.YOUNG, "щенок"),
+                Arguments.arguments(Age.ADULT, "взрослая"),
+                Arguments.arguments(Age.OLD, "старая")
+        );
+
+    }
+
+    enum Age{
+        YOUNG("молодая"),
+        ADULT("взрослая"),
+        OLD("пожилая");
+
+        private String AgeTranslate;
+
+        Age(String AgeTranslate){
+            this.AgeTranslate = AgeTranslate;
+        }
+
+        public String GetAgeTranslate(){
+            return AgeTranslate;
+        }
+    }
+
+    @ParameterizedTest
+    //@ValueSource(ints = {1, 5, 8, 4})
+    //@EnumSource(mode = EnumSource.Mode.EXCLUDE, names = {"BIG"})
+    //@EnumSource(mode = EnumSource.Mode.MATCH_ALL, names = {".*G"}) выбираем ВСЕ размеры, которые заканчиваеются на G
+    @MethodSource
+    public void ParamsTest(Size size, String translate){
+        String dog = "Jack ";
+        String breed  = "Chihuahua ";
+        System.out.println("Имя: "+ dog);
+        System.out.println("Порода: "+ breed);
+        System.out.println("Возраст: ");
+        System.out.println("Размер: " + size +" "+ size.GetSizeTranslate() + " " + translate);
+    }
+
+
+    static Stream<Arguments> ParamsTest(){
+        return Stream.of(
+                Arguments.arguments(Size.SMALL, "маленькая из статика"),
+                Arguments.arguments(Size.AVERAGE, "средняя из статика"),
+                Arguments.arguments(Size.BIG, "большая из статика")
+        );
+
+    }
+
+    enum Size{
+
+        SMALL("маленькая"),
+        AVERAGE("средняя"),
+        BIG("большая");
+
+        private String SizeTranslate;
+
+        Size(String SizeTranslate){
+            this.SizeTranslate = SizeTranslate;
+        }
+
+        public String GetSizeTranslate(){
+            return SizeTranslate;
+        }
+
     }
 
     @AfterAll
